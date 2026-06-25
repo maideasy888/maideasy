14229870913098057e52def662986b917ee15db0
maideasy888 <zygo.org888@gmail.com>
2026-06-26 06:42:21 +0800

    Initialize main.jsx with React and MaidEasy component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MaidEasy from './MaidEasy.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MaidEasy />
+  </React.StrictMode>
+);
